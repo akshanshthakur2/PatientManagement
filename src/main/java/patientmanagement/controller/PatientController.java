5c51323515fe1abf793efb30206bf4e7f989518c
akshanshthakur2 <contact.akshsnsh0@gmail.com>
2026-04-07 23:21:38 +0530

    Entities Created

---
+++ src/main/java/patientmanagement/controller/PatientController.java
@@ -0,0 +1,12 @@
+package patientmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PatientController {
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
---
+++ src/main/java/patientmanagement/entity/Doctor.java
@@ -0,0 +1,5 @@
+package patientmanagement.entity;
+
+public class Doctor {
+
+}
