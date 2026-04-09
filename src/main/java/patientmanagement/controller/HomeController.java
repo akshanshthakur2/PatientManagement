721e62c5150324356567ea7f74d2e598d19329b4
akshanshthakur2 <contact.akshsnsh0@gmail.com>
2026-04-09 13:09:59 +0530

    Fix / endpoint

--- src/main/java/patientmanagement/controller/HomeController.java
+++ src/main/java/patientmanagement/controller/HomeController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
+	@GetMapping("/index")
+	public String indexPage() {
+		return "index";
+	}
+	
+	
 	
 	
 }
